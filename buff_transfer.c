b93243ecfef8827baa1a7b33f69e7523e11ea0ac
erantanen <rantanen_ed@yahoo.com>
2012-10-29 23:26:25 -0400

    update

--- buff_transfer.c
+++ buff_transfer.c
@@ -7,19 +7,35 @@
 int  main(int argc, char *argv[])
 {
 
-
-
-  buff?
-
-
+     FILE  *filehandle_in;
+     FILE  *filehandle_out;
 
 
 
+   unsigned char *buffer_1 = (unsigned char *)malloc(20480);
+   unsigned char *buffer_2 = (unsigned char *)malloc(20480);
+
+    if(argc < 2 || argc > 4) {
+
+
+      if((filehandle_in = fopen( argv[1], "r")) == NULL) {
+           printf("\n 2 In correct usage,\n \' buff_transffer <input file> \'\n");
+                 exit(1);
+                } else {
+                     filehandle_in = fopen( argv[1], "r");
+                     filehandle_out = fopen("out_put.txt", "w");
+                }
+       
+       
 
 
 
 
 
+
+
+fclose(filehandle_in);
+fclose(filehandle_out);
 
 }
 
