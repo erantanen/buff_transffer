5915690d88ef0af42f47b481348d1013a9382a16
erantanen <rantanen_ed@yahoo.com>
2012-10-30 22:40:05 -0400

    update

--- buff_transfer.c
+++ buff_transfer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "my_buff_function.h"
+#include <stdlib.h>
+#include "my_buff_func.h"
 
 
 
@@ -11,20 +12,27 @@ int  main(int argc, char *argv[])
      FILE  *filehandle_out;
 
 
-
+// ---------------------------------------
+//  setup of memory buffers
+//----------------------------------------
    unsigned char *buffer_1 = (unsigned char *)malloc(20480);
    unsigned char *buffer_2 = (unsigned char *)malloc(20480);
+// ---------------------------------------
 
-    if(argc < 2 || argc > 4) {
+    if(argc != 2 ) {
+         printf("\n 2 In correct usage,\n \' buff_transffer <input file> \'\n");
+         exit(1);
+       }
 
 
       if((filehandle_in = fopen( argv[1], "r")) == NULL) {
            printf("\n 2 In correct usage,\n \' buff_transffer <input file> \'\n");
+           printf("\n Named file does not exit\n");
                  exit(1);
-                } else {
+       } else {
                      filehandle_in = fopen( argv[1], "r");
                      filehandle_out = fopen("out_put.txt", "w");
-                }
+       }
        
        
 
@@ -36,6 +44,7 @@ int  main(int argc, char *argv[])
 
 fclose(filehandle_in);
 fclose(filehandle_out);
-
+free(buffer_1);
+free(buffer_2);
 }
 
