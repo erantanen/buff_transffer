5aa62486ff434c830066d527c45a99ff5841070a
Bryan <bryanlea@just84.justhost.com>
2012-11-07 08:54:44 -0700

    Committer: Bryan <bryanlea@just84.justhost.com>
     On branch move
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   buff_transfer.c
    	modified:   my_buff_func.c
    	modified:   my_buff_func.h

--- my_buff_func.c
+++ my_buff_func.c
@@ -1,19 +1,23 @@
-#include "my_buff_function.h"
+#include "my_buff_func.h"
 
 
 
-void buff_read(FILE in_file, unsigned char* data_1, int d_length)
+void buff_read(unsigned char* data_1, int d_length)
 {
 
 
 
 }
 
-void buff_move(unsigned char* data_1 ,unsigned char* data_2 , int d_length)
+int buff_move(unsigned char* data_1 ,unsigned char* data_2 , int d_length, int iterator)
 {
-
-
-
+	if(iterator < d_length){
+		data_2[iterator] = data_1[iterator];
+	}
+	else{
+		iterator = -1;
+	}
+	return iterator;
 }
 
 void buff_clean(unsigned char* data_2)
--- my_buff_func.h
+++ my_buff_func.h
@@ -1,9 +1,11 @@
 #ifndef MY_BUFF_FUNC_H
 #define MY_BUFF_FUNC_H
 
-void buff_read(char, int);
-void buff_move(unsigned char,unsigned char, int);
-void buff_clean(unsigned char);
+void buff_read(unsigned char*, int);
+
+int buff_move(unsigned char*, unsigned char*, int, int);
+
+void buff_clean(unsigned char*);
 
 
 
